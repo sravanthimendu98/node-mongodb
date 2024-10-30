284f153e0ae7600c98eb86ada3b170701a369d15
sravanthi mendu <sravanthi.mendu98@gmail.com>
2024-10-30 15:55:35 +0530

    Added template engines -- pug and ejs

--- node/server.js
+++ node/server.js
@@ -5,12 +5,25 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// database connection
 connectDB();
 
+// Set pug/ejs as the view engine
+// app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Routes
 app.use("/users", userRoutes);
+
+// Route to render the Pug template
+app.get("/", (req, res) => {
+    res.render("index", { title: "Welcome to My App", message: "Hello, EJS!" });
+});
 
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
